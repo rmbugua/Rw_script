70097fee0932491e7003c38241c73f6d043018b9
rose <wanjikurose34@gmail.com>
2022-10-16 20:56:03 +0300

    project_6

--- rw-script.sh
+++ rw-script.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+sudo yum update -y
+sudo yum install git -y
+sudo yum install wget -y
+sudo yum install net-tools -y
+sudo yum install docker -y
+sudo useradd Rose
+sudo useradd Josh
+sudo useradd Peshu
+sudo useradd Fred
+sudo useradd Rita
+sudo groupadd Kenya-devops
+sudo groupadd Uganda-devops
